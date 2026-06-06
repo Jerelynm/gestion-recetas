b8d298a1463e1fe5fd4a4c8482be76138426edda
Jerelynm <jmarinm4@miumg.edu.gt>
2026-06-06 10:53:18 -0600

    se modifican los seeders

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Recipe extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'category',
+        'preparation_time',
+        'chef_id',
+    ];
+
+    public function chef()
+    {
+        return $this->belongsTo(Chef::class);
+    }
 }
--- marin_examen_recetas/database/seeders/DatabaseSeeder.php
+++ marin_examen_recetas/database/seeders/DatabaseSeeder.php
@@ -2,24 +2,23 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Chef;
+use App\Models\Recipe;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Chef::factory()
+            ->count(10)
+            ->create()
+            ->each(function ($chef) {
+                Recipe::factory()
+                    ->count(3)
+                    ->create([
+                        'chef_id' => $chef->id,
+                    ]);
+            });
     }
 }
